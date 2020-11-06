fa65c8fb07ecf826ca95d4601e36cf5930c25c4c
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-06 22:29:57 +0500

    feat(Home): create panel

---
+++ src/panels/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { arrayOf, shape, string, func } from 'prop-types';
+
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+
+import Icon24Filter from '@vkontakte/icons/dist/24/filter';
+
+const Home = ({ fetchedFriends, onOpenModal }) => {
+  return (
+    <Panel>
+      <PanelHeader>
+        Friends
+      </PanelHeader>
+      <PanelHeaderButton onClick={() => onOpenModal("filters")}>
+        <Icon24Filter />
+      </PanelHeaderButton>
+      {fetchedFriends && (
+        <Group title="Friends list">
+          {
+            fetchedFriends.map((friend, i) => {
+              return (
+                <Cell
+                  key={`${i}-${friend.id}`}
+                  before={friend['photo_100'] ? <Avatar src={friend['photo_100']}/> : null}
+                >
+                  {`${friend['first_name']} ${friend['last_name']}`}
+                </Cell>
+              )
+            })
+          }
+        </Group>
+      )}
+  </Panel>
+  )
+};
+
+Home.propTypes = {
+  fetchedFriends: arrayOf(shape({
+    photo_100: string,
+    first_name: string,
+    last_name: string,
+  })),
+  onOpenModal: func.isRequired,
+};
+
+export default Home;
