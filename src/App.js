90312af73f3ed4902369bb2bff3be3111dc88391
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-07 23:25:29 +0500

    feat(App): new logics fetch friends. WIP

--- src/App.js
+++ src/App.js
@@ -13,16 +13,85 @@ import '@vkontakte/vkui/dist/vkui.css';
 import Home from './panels/Home';
 import Filters from './panels/Filters';
 
-const ACCESS_TOKEN = '3a37ccad3a37ccad3a37ccada43a4308a533a373a37ccad6593f773c884d7889fcbfb66';
-const COUNT_FRIENDS = 10000;
-const COUNT_REQUEST_EXECUTE = 25;
+const ACCESS_TOKEN = '';
+const COUNT_FRIENDS = 1500;
+const COUNT_REQUEST_EXECUTE = 10;
+
+const timeoutPromise = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
 
 const App = () => {
   const [userId, setUserId] = useState(null);
-  const [fetchedFriends, setFetchedFriends] = useState(null);
-  const [fetchedFriendsId, setFetchedFriendsId] = useState(null);
+  const [friendsCurrentUser, setFriendsCurrentUser] = useState(null);
+  const [friends, setFriends] = useState([]);
+  const [friendsId, setIdFriends] = useState([]);
+  const [fetchedUsers, setFetchedUsers] = useState([]);
   const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
   const [activeModal, setActiveModal] = useState(null);
+
+  const getFriends = async (friendsUser, authToken) => {
+
+    const activeFriends = friendsUser.filter((friend) => {
+      if (!friend.hasOwnProperty('deactivated')) {
+        return friend;
+      }
+    });
+
+    const friendsId = activeFriends.map((friend) => {
+      return friend.id;
+    })
+
+    let startSlice = 0;
+    let finishSlice = COUNT_REQUEST_EXECUTE;
+
+    const uniqueId = [];
+    const uniqueFriends = [];
+
+    while (uniqueId.length < COUNT_FRIENDS) {
+      if (finishSlice > friendsId.length) {
+        finishSlice = friendsId.length - 1;
+      }
+      const friendsListString = friendsId.slice(startSlice, finishSlice).join();
+      await timeoutPromise(335);
+      startSlice += COUNT_REQUEST_EXECUTE;
+      finishSlice += COUNT_REQUEST_EXECUTE;
+
+      const friendsFetched = await bridge.send("VKWebAppCallAPIMethod", {
+        "method": "execute",
+        "request_id": "friends",
+        "params": {
+          "code":  `var friends = [${friendsListString}];
+                    var fetchedFriends = [];
+                    var count = 0;
+                    while (count < 25) {
+                      var fetchedFriend = API.friends.get({
+                        "user_id": friends[count],
+                        "fields": "first_name, last_name, sex, bdate, photo_100",
+                      });
+                  
+                      count = count + 1;
+                      fetchedFriends.push(fetchedFriend.items);
+                    };
+                    return fetchedFriends;`,
+          "v":"5.124", 
+          "access_token": authToken.access_token,
+    }});
+
+      friendsFetched.response.flat().forEach((friend) => {
+        if(!uniqueId.includes(friend.id)) {
+          uniqueId.push(friend.id);
+          uniqueFriends.push(friend);
+        }
+      })
+
+      setIdFriends(uniqueId);
+
+      if (startSlice > finishSlice) {
+        break;
+      }
+    }
+
+    return uniqueFriends;
+  }
 
   const modal = (
     <ModalRoot activeModal={activeModal}>
@@ -45,7 +114,6 @@ const App = () => {
       </ModalPage>
     </ModalRoot>
   )
-
   useEffect(() => {
     bridge.subscribe(({ detail: { type, data }}) => {
       if (type === 'VKWebAppUpdateConfig') {
@@ -58,70 +126,53 @@ const App = () => {
     async function fetchData() {
       const user = await bridge.send('VKWebAppGetUserInfo');
       setUserId(user.id);
-      setPopout(null);
+      setFetchedUsers(fetchedUsers.concat([user.id]));
     }
 
     fetchData();
   }, []);
 
+  useEffect(() => {
+    async function fetchFriendsUser() {
+      if (userId) {
+        const friendsUser = await bridge.send("VKWebAppCallAPIMethod", {
+          "method": "friends.get",
+          "request_id": "friends_current_user",
+          "params": {
+            "user_id": userId,
+            "v":"5.124", 
+            "access_token": ACCESS_TOKEN,
+            "fields": "first_name, last_name, sex, bdate, photo_100",
+          }
+        });
+  
+        setFriendsCurrentUser(friendsUser)
+      }
+    }
+
+    fetchFriendsUser()
+  }, [userId, friends])
+
 
   useEffect(() => {
     async function fetchFriends() {
-        if (userId) {
-          const authToken = await bridge.send("VKWebAppGetAuthToken", {
-            "app_id": 7652360,
-            "scope": ""
-          });
+      const authToken = await bridge.send("VKWebAppGetAuthToken", {
+        "app_id": 7652360,
+        "scope": ""
+      });
 
-          const friendsUser = await bridge.send("VKWebAppCallAPIMethod", {
-            "method": "friends.get",
-            "request_id": "friends_current_user",
-            "params": {
-              "user_id": userId,
-              "v":"5.124", 
-              "access_token": ACCESS_TOKEN,
-              "fields": "first_name, last_name, sex, bdate, photo_100",
-            }
-          })
-
-          setFetchedFriends(friendsUser.response.items);
-
-        // Превышен лимит execute
-        
-        //   const isNotEmptyFriendsList = friendsUser.response.items.length;
-
-        //   if (isNotEmptyFriendsList) {
-        //     const friendsList = friendsUser.response.items;
-        //     const friendsListString = friendsList.slice(0, COUNT_REQUEST_EXECUTE).join();
-        //     console.log(friendsListString)
-        //     const friendsFetched = await bridge.send("VKWebAppCallAPIMethod", {
-        //       "method": "execute",
-        //       "request_id": "friends",
-        //       "params": {
-        //         "code":  `var friends = [${friendsListString}];
-        //                   var fetchedFriends = [];
-        //                   var count = 0;
-        //                   while (count < 25) {
-        //                     var fetchedFriend = API.friends.get({
-        //                       "user_id": friends[count],
-        //                       "fields": "first_name, last_name, sex, bdate, photo_100",
-        //                     });
-                        
-        //                     count = count + 1;
-        //                     fetchedFriends.push(fetchedFriend.items);
-        //                   };
-        //                   return fetchedFriends;`,
-        //         "v":"5.124", 
-        //         "access_token": authToken.access_token,
-        //   }});
-
-        //   console.log(friendsFetched)
-        // }
+      if (friendsCurrentUser) {
+        if (friendsCurrentUser.response.items.length) {
+          const fetchFriends = await getFriends(friendsCurrentUser.response.items, authToken);
+          setFriends(fetchFriends.slice(0, COUNT_FRIENDS));
+  
+          setPopout(null);
+        }
       }
     }
 
     fetchFriends();
-  }, [userId])
+  }, [friendsCurrentUser])
 
   return (
     <View
@@ -130,7 +181,7 @@ const App = () => {
     >
       <Home
         onOpenModal={setActiveModal}
-        fetchedFriends={fetchedFriends}
+        friends={friends}
       />
     </View>
   );
